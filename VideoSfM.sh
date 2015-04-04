a93f2cae23e285303ba59c5f2db094fa5f4c9ca6
Joe Barrow <joseph.d.barrow@gmail.com>
2015-04-04 13:05:20 -0400

    Generated both types of files

--- VideoSfM.sh
+++ VideoSfM.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+# We want to do eht same thing twice -- once for pgm so the
+# KLT feature tracker can match points and once for ppm
+# so our point cloud can have color points
+ffmpeg -i $1 -r $3 -f image2 $2/image-%07d.pgm
 ffmpeg -i $1 -r $3 -f image2 $2/image-%07d.ppm
